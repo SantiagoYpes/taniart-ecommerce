81673d29a104cf44d4f343a1b3aa67db47d9fa37
SantiagoYpes <santiyezu8@hotmail.com>
2025-06-24 16:42:34 -0500

    Add GridItem

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import { CheckOutFlow } from './components/CheckOutFlow'
+import Box from '@mui/material/Box';
+import { GridItem } from './styles/GridItem';
+import Grid from '@mui/material/Grid';
 const page = () => {
   return (
     <div>
-        <CheckOutFlow/>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid size={8}>
+            <GridItem>size=8</GridItem>
+          </Grid>
+          <Grid size={4}>
+            <GridItem><CheckOutFlow /></GridItem>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   )
 }
---
+++ src/app/checkout/styles/GridItem.tsx
@@ -0,0 +1,15 @@
+
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+
+
+export const GridItem = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: 'gray',
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#1A2027',
+  }),
+}));
